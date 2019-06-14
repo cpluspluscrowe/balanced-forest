5dcc7305ef0f375e665381794fca9f0ad9aedfcc
Chad Crowe <ccrowe@linkedin.com>
2019-06-14 10:19:54 -0500

    Got get-forest working

--- src/balanced_forest/core.clj
+++ src/balanced_forest/core.clj
@@ -6,9 +6,20 @@
   (let [p1 (first edge)
         p2 (second edge)]
     (hash-map p1 p2 p2 p1)))
+(defn get-hash-map-value-as-list [key hm]
+  (let [value (get hm key)]
+    (if (list? value) value
+        (list value))))
+
+(defn merge-hash-maps-at-key [key hm1 hm2]
+  (hash-map key (filter identity (reduce conj (get-hash-map-value-as-list key hm1) (get-hash-map-value-as-list key hm2)))))
+
+(defn merge-hash-maps [hm1 hm2]
+  (let [keys (reduce conj (keys hm1) (keys hm2))]
+    (reduce merge (map #(merge-hash-maps-at-key % hm1 hm2) keys))))
 
 (defn edges-to-dict [edges]
-  (reduce merge (map edge-to-hash-map edges)))
+  (reduce merge-hash-maps (map edge-to-hash-map edges)))
 
 (defn get-forest
   ([edge edges] (get-forest edge edges (hash-set edge)))
--- test/balanced_forest/core_test.clj
+++ test/balanced_forest/core_test.clj
@@ -12,7 +12,18 @@
       (is (= (edges-to-dict edges) (hash-map 1 2 2 1)
              )))))
 
+(deftest edges-to-dict-multiple-test
+  (testing "converting a list of edges into a dictionary"
+    (let [edges (list (list 1 2) (list 1 3))]
+      (is (= (edges-to-dict edges) (hash-map 1 (list 3 2) 3 (list 1) 2 (list 1)
+                                             ))))))
+
 (deftest get-forest-test
   (testing "Getting a set of all nodes connected to the edge"
     (is (= (get-forest 1 (list (list 1 2))) (hash-set 2)))
     ))
+
+;(deftest get-forest-test-larger
+;  (testing "Getting a set of all nodes connected to the edge"
+;    (is (= (get-forest 1 (list (list 1 2) (list 1 3))) (hash-set 2 3)))
+;    ))
